fd23ea7877479837b42b6966df7f2bd47f08849c
dinal-qlub <pasindu.dinal@qlub.io>
2025-03-19 17:17:26 +0530

    fix: main port

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 
 	logger.Info("Starting server on :8080")
 	http.HandleFunc("/upload", handleUpload)
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		logger.Fatal("Failed to start server", zap.Error(err))
 	}
 }
